fc03053ad00f7b3ce114aeed28bc494284598e28
drewlinsley <drewlinsley@gmail.com>
2017-08-02 21:04:07 -0400

    Added classes for cifar and mnist data loading.

---
+++ config.py
@@ -0,0 +1,5 @@
+
+class config():
+    def __init__(self, **kwargs):
+        self.data_root = '/media/data_cifs/image_datasets/'
+
--- dataset_processing/cifar.py
+++ dataset_processing/cifar.py
@@ -1,1 +1,39 @@
-cifar.py
+import os
+import re
+from glob import glob
+from config import config
+
+
+class data_processing(object):
+    def __init__(self):
+        self.name = 'cifar'
+        self.extension = '.png'
+        self.config = config()
+        self.folds = {
+            'train': 'train',
+            'test': 'test']
+
+    def __enter__(self):
+        files = self.get_files()
+        labels = self.get_labels(files)
+        return files, labels
+
+    def get_files(self):
+        files = {}
+        for k, fold in self.folds.iteritems():
+            files[k] = glob(
+                os.path.join(
+                    self.config.data_root,
+                    fold,
+                    '*%s' % self.extension))
+        return files
+
+    def get_labels(self, files):
+        labels = {}
+        for k, v in files.iteritems():
+            it_labels = []
+            for f in v:
+                it_labels += [int(re.split('\.', f.split('_')[-1])[0])]
+            labels[k] = it_labels
+        return labels
+
