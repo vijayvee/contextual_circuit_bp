743402098c9aba5d69fe4444dbd353161d0d5c42
Drew Linsley <drewlinsley@gmail.com>
2017-09-04 19:09:06 -0400

    Updated experiments.

---
+++ models/structs/cluster_one_layer_rnns_selective/divisive.py
@@ -0,0 +1,18 @@
+
+
+layer_structure = [
+    {
+        'layers': ['conv'],
+        'weights': [64],
+        'names': ['conv1_1'],
+        'filter_size': [3],
+        'normalization': ['divisive_2d'],
+        'normalization_target': ['post'],
+    },
+    {
+        'layers': ['pool'],
+        'weights': [None],
+        'names': ['pool2'],
+        'filter_size': [None]
+    }
+]
