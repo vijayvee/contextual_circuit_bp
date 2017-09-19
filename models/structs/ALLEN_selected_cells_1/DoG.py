85c68bf46d4fe74e9aca810c2e6e3c20616aa373
Drew Linsley <drewlinsley@gmail.com>
2017-09-19 13:46:52 -0400

    Transfer to x9.

---
+++ models/structs/ALLEN_selected_cells_1/DoG.py
@@ -0,0 +1,18 @@
+"""DoG model for Allen data."""
+
+layer_structure = [
+    {
+        'layers': ['DoG'],
+        'weights': [9],
+        'names': ['conv1_1'],
+        'filter_size': [7],
+        'activation': ['logistic'],
+        'normalization_target': ['post'],
+    },
+    {
+        'layers': ['pool'],
+        'weights': [None],
+        'names': ['pool1'],
+        'filter_size': [None]
+    }
+]
---
+++ models/structs/ALLEN_selected_cells_1/conv2d.py
@@ -0,0 +1,18 @@
+"""2D convolutional model for Allen data."""
+
+layer_structure = [
+    {
+        'layers': ['conv'],
+        'weights': [64],
+        'names': ['conv1_1'],
+        'filter_size': [7],
+        'activation': ['relu'],
+        'normalization_target': ['post'],
+    },
+    {
+        'layers': ['pool'],
+        'weights': [None],
+        'names': ['pool1'],
+        'filter_size': [None]
+    }
+]
